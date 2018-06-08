fe909cac055021656628f9a0646f01dd4a59a88c
pxl8108 <parthasarathi_logakannan@homedepot.com>
2018-06-08 09:12:43 -0400

    Created a base HelloWorld example for Kotlin

---
+++ HelloWorld/src/Main.kt
@@ -0,0 +1,6 @@
+/**This method used as a starting point for kotlin framework.
+ * It creates MainKT class at compile time. It can be changes with @JVMName annotation.
+ */
+fun main(args : Array<String>){
+println("Hello World !!!")
+}
